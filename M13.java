44f52fba32f2d56db54bdcab0938aec237884538
Manikanta-CH-git <manikanta.23cs064@sode-edu.in>
2024-11-05 15:51:24 +0530

    Add files via upload

---
+++ M13.java
@@ -0,0 +1,24 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class M13 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File f1=new File("D:\\4MW23CS064\\OOPJ\\src\\names.txt");
+		Scanner sc1=new Scanner(f1);
+		String name="";
+		Tools1 t1=new Tools1();
+		
+		name=sc1.nextLine();
+		System.out.println(t1.reverse(name));
+		
+		name=sc1.nextLine();
+		System.out.println(t1.reverse(name));
+		
+		name=sc1.nextLine();
+		System.out.println(t1.reverse(name));
+
+	}
+
+}
